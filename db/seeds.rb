8941431968a036ea6e1d9a4ab14e68d45f9b2876
Arios16 <agrios@uc.cl>
2017-04-27 21:20:08 -0300

    arreglado seeding para que no genere datos random en produccion (oops)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,78 +5,81 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create!(name: Faker::StarWars.character,
-             email: 'orrios@uc.cl',
-             username: Faker::Internet.user_name,
-             last_name: Faker::StarWars.specie,
-             phone_number: Faker::PhoneNumber.cell_phone,
-             is_admin: true,
-             address: Faker::StarWars.wookie_sentence,
-             password: '123456')
-
-5.times do
+case Rails.env
+when 'development'
   User.create!(name: Faker::StarWars.character,
-               email: Faker::Internet.unique.email,
+               email: 'orrios@uc.cl',
                username: Faker::Internet.user_name,
                last_name: Faker::StarWars.specie,
                phone_number: Faker::PhoneNumber.cell_phone,
                is_admin: true,
                address: Faker::StarWars.wookie_sentence,
                password: '123456')
-end
 
-Product.create!(name: 'Notebook',
-                prize: 500,
-                stock: 3,
-                description: Faker::Lorem.paragraph(2),
-                details: Faker::Lorem.paragraph)
+  5.times do
+    User.create!(name: Faker::StarWars.character,
+                 email: Faker::Internet.unique.email,
+                 username: Faker::Internet.user_name,
+                 last_name: Faker::StarWars.specie,
+                 phone_number: Faker::PhoneNumber.cell_phone,
+                 is_admin: true,
+                 address: Faker::StarWars.wookie_sentence,
+                 password: '123456')
+  end
 
-5.times do
-  Product.create!(name: Faker::Commerce.product_name,
-                  prize: Random.rand(1..10_000),
-                  stock: Random.rand(10..20),
+  Product.create!(name: 'Notebook',
+                  prize: 500,
+                  stock: 3,
                   description: Faker::Lorem.paragraph(2),
                   details: Faker::Lorem.paragraph)
-end
 
-fact = Faker::ChuckNorris.fact[0...-1]
+  5.times do
+    Product.create!(name: Faker::Commerce.product_name,
+                    prize: Random.rand(1..10_000),
+                    stock: Random.rand(10..20),
+                    description: Faker::Lorem.paragraph(2),
+                    details: Faker::Lorem.paragraph)
+  end
 
-Question.create!(content: "is it true that #{fact}?",
-                 user_id: 1,
-                 product_id: 1)
+  fact = Faker::ChuckNorris.fact[0...-1]
 
-QuestionVote.create!(user_id: 1,
-                     question_id: 1,
+  Question.create!(content: "is it true that #{fact}?",
+                   user_id: 1,
+                   product_id: 1)
+
+  QuestionVote.create!(user_id: 1,
+                       question_id: 1,
+                       delta: 1)
+
+  QuestionVote.create!(user_id: 2,
+                       question_id: 1,
+                       delta: -1)
+
+  QuestionVote.create!(user_id: 3,
+                       question_id: 1,
+                       delta: -1)
+
+  Answer.create!(content: "yes, its true that #{fact}",
+                 question_id: 1,
+                 user_id: 1)
+
+  AnswerVote.create!(user_id: 1,
+                     answer_id: 1,
                      delta: 1)
 
-QuestionVote.create!(user_id: 2,
-                     question_id: 1,
+  AnswerVote.create!(user_id: 2,
+                     answer_id: 1,
+                     delta: 1)
+
+  AnswerVote.create!(user_id: 3,
+                     answer_id: 1,
                      delta: -1)
 
-QuestionVote.create!(user_id: 3,
-                     question_id: 1,
-                     delta: -1)
+  Order.create!(user_id: 1,
+                status: :processing)
 
-Answer.create!(content: "yes, its true that #{fact}",
-               question_id: 1,
-               user_id: 1)
-
-AnswerVote.create!(user_id: 1,
-                   answer_id: 1,
-                   delta: 1)
-
-AnswerVote.create!(user_id: 2,
-                   answer_id: 1,
-                   delta: 1)
-
-AnswerVote.create!(user_id: 3,
-                   answer_id: 1,
-                   delta: -1)
-
-Order.create!(user_id: 1,
-              status: :processing)
-
-OrderedProduct.create!(order_id: 1,
-                        product_id: 1,
-                        quantity: 5)
+  OrderedProduct.create!(order_id: 1,
+                         product_id: 1,
+                         quantity: 5)
+when 'production'
+end
