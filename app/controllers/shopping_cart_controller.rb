46bc2d80ba0241d7eac60cf0341d0b3361594a8f
Arios16 <agrios@uc.cl>
2017-05-13 19:00:28 -0300

    agregado poder agregar y quitar items del carro

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -3,27 +3,27 @@ class ShoppingCartController < ApplicationController
 
   def add_item
     pid = cart_params[:product_id]
+    p cart_params
     @cart[pid] = (@cart[pid] || 0) + cart_params[:quantity]
-    flash[:notice] = "This product was successfully added to your Shopping Cart"
-    redirect_to :back
+    flash[:notice] = 'This product was successfully added to your Shopping Cart'
+    redirect_back(fallback_location: root_path)
   end
 
   def remove_item
     @cart.delete(cart_params[:product_id])
-    flash[:notice] = "The item was successfully removed from your Shopping Cart"
-    redirect_to :back
+    flash[:notice] = 'The item was successfully removed from your Shopping Cart'
+    redirect_back(fallback_location: root_path)
   end
 
   def adjust_quantity
     @cart[cart_params[:product_id]] = cart_params[:quantity]
-    flash[:notice] = "Your Shopping Cart has been updated successfuly"
-    redirect_to :back
+    flash[:notice] = 'Your Shopping Cart has been updated successfuly'
+    redirect_back(fallback_location: root_path)
   end
 
   def show
-    @products = []
-    @cart.each do |key, value|
-      @products.push([Product.find(key),value])
+    @products = Product.find(@cart.keys).map do |p|
+      [p, @cart[p.id.to_s]]
     end
   end
 
