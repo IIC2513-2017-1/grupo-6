f9e67a90240473f36097a6094623946c6cb621c5
orrios <orrios@uc.cl>
2017-05-25 10:50:49 -0400

    fixed slider multiple image passing, added review count to star display

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,94 +2,98 @@
 // All this logic will automatically be available in application.js.
 
 $(document).on('turbolinks:load', function () {
-  var $sliderWrap = $('#slider-wrap'),
-      $slider = $sliderWrap.find('.slides'),
-      $firstSlide = $slider.find('li:first'),
-      $lastSlide = $slider.find('li:last'),
-      $sliderSlides = $sliderWrap.find('.slides > li'),
-      $navDots = $sliderWrap.find('.nav-dots li'),
-      sliderWidth = $sliderWrap.width(),
-      // dynamic variables
-      sliderPos = 0,
-      s_index = 0,
-      currentSlide = $sliderSlides[s_index];
+    var $sliderWrap = $('#slider-wrap'),
+        $slider = $sliderWrap.find('.slides'),
+        $firstSlide = $slider.find('li:first'),
+        $lastSlide = $slider.find('li:last'),
+        $sliderSlides = $sliderWrap.find('.slides > li'),
+        $navDots = $sliderWrap.find('.nav-dots li'),
+        sliderWidth = $sliderWrap.width(),
+        // dynamic variables
+        sliderPos = 0,
+        s_index = 0,
+        currentSlide = $sliderSlides[s_index];
 
-  function changeSlide() {
-      $slider.css('left', sliderPos);
+    function changeSlide() {
+        $slider.css('left', sliderPos);
 
-      $navDots.removeClass('active');
-      $($navDots[s_index]).addClass('active');
-  }
+        $navDots.removeClass('active');
+        $($navDots[s_index]).addClass('active');
+    }
 
 
-  function setSliderWidth() {
-      /* -----------------------------------------
-      * set slider width
-      * slider container * number of slides
-      * ----------------------------------------- */
-      $slider.css('width', sliderWidth * $sliderSlides.length);
-  }
+    function setSliderWidth() {
+        /* -----------------------------------------
+         * set slider width
+         * slider container * number of slides
+         * ----------------------------------------- */
+        $slider.css('width', sliderWidth * $sliderSlides.length);
+    }
 
-  function nextSlide() {
-      /* -----------------------------------------
-      * IF: slide index >= total slides
-      * THEN:
-      *      slide index = 0 
-      *      slide pos = 0
-      * ELSE:
-      *      slide index + 1
-      *      move slider position one slide
-      * ---------------------------------------- */
-      s_index >= ($sliderSlides.length - 1) ? (
-          // s_index = 0,
-          // sliderPos = 0,
-          false
-      ) : (
-          s_index++,
-          sliderPos -= sliderWidth
-      );
+    function nextSlide() {
+        /* -----------------------------------------
+         * IF: slide index >= total slides
+         * THEN:
+         *      slide index = 0 
+         *      slide pos = 0
+         * ELSE:
+         *      slide index + 1
+         *      move slider position one slide
+         * ---------------------------------------- */
+        s_index >= ($sliderSlides.length - 1) ? (
+            // s_index = 0,
+            // sliderPos = 0,
+            false
+        ) : (
+            s_index++,
+            sliderPos -= sliderWidth
+        );
 
-      // change slide
-      changeSlide();
-  }
+        // change slide
+        changeSlide();
+    }
 
-  function prevSlide() {
-      /* -----------------------------------------
-      * IF: slide index <= 0
-      * THEN:
-      *      slide index = $slides.length
-      *      move slider position to last slide
-      * ELSE:
-      *      slide index - 1
-      *      move slider position back one slide
-      * ---------------------------------------- */
-      s_index <= 0 ? (
-          // s_index = ($sliderSlides.length - 1),
-          // sliderPos = -$sliderWrap.width()
-          false
-      ) : (
-          s_index--,
-          sliderPos += sliderWidth
-      );
+    function prevSlide() {
+        /* -----------------------------------------
+         * IF: slide index <= 0
+         * THEN:
+         *      slide index = $slides.length
+         *      move slider position to last slide
+         * ELSE:
+         *      slide index - 1
+         *      move slider position back one slide
+         * ---------------------------------------- */
+        s_index <= 0 ? (
+            // s_index = ($sliderSlides.length - 1),
+            // sliderPos = -$sliderWrap.width()
+            false
+        ) : (
+            s_index--,
+            sliderPos += sliderWidth
+        );
 
-      // change slide
-      changeSlide();
-  }
+        // change slide
+        changeSlide();
+    }
 
-  // set slider width
-  setSliderWidth();
+    // set slider width
+    setSliderWidth();
 
-  $navDots.on('click', function(e) {
-      var oldIndex = s_index,
-          curIndex = $navDots.index(this);
-
-      /* -----------------------------------------
-      * IF: curIndex = oldIndex
-      * THEN: return false
-      * ELSE IF: oldIndex < curIndex
-      * THEN: nextSlide()
-      * ELSE: prevSlide()
-      * ----------------------------------------- */
-      curIndex === oldIndex ? false : (oldIndex < curIndex) ? nextSlide() : prevSlide();
-  });
+    $navDots.on('click', function (e) {
+        var oldIndex = s_index,
+            curIndex = $navDots.index(this);
+            
+        if (curIndex === oldIndex) {
+            return false
+        } else if (oldIndex < curIndex) {
+            for (var i = curIndex - oldIndex - 1; i > 0; i--) {
+                nextSlide()
+            }
+        } else {
+            for (var i = oldIndex - curIndex - 1; i > 0; i--) {
+                prevSlide()
+            }
+        }
+        curIndex === oldIndex ? false : (oldIndex < curIndex) ? nextSlide() : prevSlide();
+    });
 });
