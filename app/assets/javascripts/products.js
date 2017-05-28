7ee1fe85fb73387f2685c7413af7925884c96ee1
orrios <orrios@uc.cl>
2017-05-27 21:12:56 -0400

    added tabs

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -143,4 +143,28 @@ $(document).on('turbolinks:load', function () {
 inputNumber($('.input-number'));
 
 
-});
+// tabs js
+
+$('.tablinks').on('click', function() {
+     var link = $(this);
+     var tab = $('.' + link.data('tab'));
+ 
+     // Get all elements with class="tabcontent" and hide them
+     $('.tabcontent').each(function() {
+       $(this).hide();
+     });
+ 
+     // Get all elements with class="tablinks" and remove the class "active"
+     $('.tablinks').each(function() {
+       $(this).removeClass('active');
+     });
+ 
+     // Show the current tab, and add an "active" class to the button that opened the tab
+     tab.show();
+     link.addClass('active');
+   });
+   $('.defaultOpen').click();
+ });
+
+
+
