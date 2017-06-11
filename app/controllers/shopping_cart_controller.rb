cf79bd8fc39b2d3f875a53b9d84d751ca7d04a93
Arios16 <agrios@uc.cl>
2017-06-11 13:25:20 -0400

    Implementado funcionalidades del carrito mediante AJAX

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -6,25 +6,41 @@ class ShoppingCartController < ApplicationController
     if quantity > 0
       pid = cart_params[:product_id]
       @cart[pid] = (@cart[pid] || 0) + cart_params[:quantity]
-      flash[:notice] = 'This product was successfully added to your Shopping Cart'
     end
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html do
+        flash[:notice] = 'This product was successfully added to your Shopping Cart'
+        redirect_back(fallback_location: root_path)
+      end
+      format.js
+    end
   end
 
   def remove_item
-    @cart.delete(cart_params[:product_id])
-    flash[:notice] = 'The item was successfully removed from your Shopping Cart'
-    redirect_back(fallback_location: root_path)
+    @product_id = cart_params[:product_id]
+    @cart.delete(@product_id)
+    respond_to do |format|
+      format.html do
+        flash[:notice] = 'The item was successfully removed from your Shopping Cart'
+        redirect_back(fallback_location: root_path)
+      end
+      format.js
+    end
   end
 
   def adjust_quantity
     if cart_params[:quantity] > 0
       @cart[cart_params[:product_id]] = cart_params[:quantity]
-      flash[:notice] = 'Your Shopping Cart has been updated successfuly'
     else
       @cart.delete(cart_params[:product_id])
     end
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html do
+        flash[:notice] = 'Your Shopping Cart has been updated successfuly'
+        redirect_back(fallback_location: root_path)
+      end
+      format.js
+    end
   end
 
   def show
