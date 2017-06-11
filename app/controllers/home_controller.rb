5ecadfc5b22a0690bd0d4001e3a86afc396d90bb
orrios <orrios@uc.cl>
2017-06-11 17:57:06 -0400

    add correct graph sales by category past 30 days

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,37 @@ class HomeController < ApplicationController
     @tags = Tag.all
     @users = User.all
     @coupons = Coupon.all
+
+    @last_orders = @orders.where('created_at > ?', 29.days.ago)
+    
+    @cats = @categories.map{ |category| [category.name, []] }.to_h
+    amount_sold = 0
+    @cats.keys.each do |cat|
+      amount_sold = 0
+      (Date.today-30.days).upto(Date.today) do |date|
+        amount_sold = 0
+        created_orders = @last_orders.where(created_at: date.midnight..date.end_of_day)
+        created_orders.each do |o|
+          ordered_product = o.ordered_products
+
+          ordered_product.each do |o_product|
+            quantity = o_product.quantity
+            price = o_product.product.prize
+            if o_product.product.categories.map{|c| c.name}.include?(cat)
+              amount_sold = amount_sold + quantity*price                        
+            end
+          end
+        end
+        @cats[cat].push(amount_sold)
+      end    
+    end
+
+    @cats = @cats.map{ |key, value| {name: key, data: value} }
+
+    p @cats
+
   end
+  
+
   
 end
